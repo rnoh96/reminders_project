b44b47e6793e978933d302dae6b4d77f554951e6
apu-pixel <62687887+apu-pixel@users.noreply.github.com>
2020-11-22 21:54:41 -0800

    Add files via upload

---
+++ controller/auth_controller.js
@@ -0,0 +1,22 @@
+let database = require("../database");
+
+let authController = {
+  login: (req, res) => {
+    res.locals.page = "login"
+    res.render('auth/login')
+  },
+
+  register: (req, res) => {
+    res.render('auth/register')
+  },
+
+  loginSubmit: (req, res) => {
+    // implement
+  },
+
+  registerSubmit: (req, res) => {
+    // implement
+  }
+}
+
+module.exports = authController;
